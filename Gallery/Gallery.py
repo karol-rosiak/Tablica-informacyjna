0d6e7f8e45f23ee8311c7ad0a2cf5ac3fdeafece
karol-rosiak <rosiak-karol@wp.pl>
2019-04-17 14:55:53 +0200

    v2

--- Gallery/Gallery.py
+++ Gallery/Gallery.py
@@ -1,19 +1,41 @@
 import cv2
-import glob
-import time
-image_list = []
-for filename in glob.glob('C:\\Users\\Nico\\Desktop\\WP/*.jpg'):
-    im = cv2.imread(filename)
-    image_list.append(im)
+import time as t
+import scheduler
+
+file = 'C:\\Users\\Karol\\Desktop\\screen\\'
+
+
+def video(filename):
+    cap = cv2.VideoCapture(file+filename)
+
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        print(ret)
+        if ret:
+            cv2.imshow('Gallery', frame)
+            if cv2.waitKey(33) == 27:
+                exit()
+                break
+        else:
+            break
+    cap.release()
+
+def img(filename,time):
+    cv2.imshow('Gallery', cv2.imread(file+filename))
+    if cv2.waitKey(33) == ord('q'):
+        exit()
+    t.sleep(int(time))
+
 i = 0
 cv2.namedWindow('Gallery', cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty('Gallery', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+l=scheduler.scheduleMedia()
+print(l)
 while True:
-        cv2.imshow('Gallery', image_list[i])
-        time.sleep(3)
-        i += 1
-        if image_list.__len__() == i:
-            i = 0
-        if cv2.waitKey(33) == ord('q'):
-            exit()
+    for x in l:
+        if x["type"] == 'image':
+            img(x["name"], x["duration"])
+        if x["type"] == 'video':
+            video(x["name"])
 
